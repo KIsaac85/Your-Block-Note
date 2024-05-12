6a26dcce4138ddc5323f982fefce68962c3ed169
Karim <karim_isaac@hotmail.com>
2024-05-12 15:24:17 +0300

    main commit main

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port =3000;
+const obj = {
+  userInput:[]
+}
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function getUserInput(res,req) {
+  let input = req.body['uiInput']
+  if (!obj.userInput.includes(input)) {
+    obj.userInput.push(input)
+  }
+}
+app.get("/", (req, res) => {
+    res.render('index.ejs')
+  });
+  app.get("/work", (req, res) => {
+    res.render('work.ejs')
+  });
+
+app.post("/add", (req, res) => {
+  getUserInput(res,req)
+    res.render('index.ejs',obj)
+
+});
+app.post("/work", (req, res) => {
+  getUserInput(res,req)
+    res.render('work.ejs',obj)
+
+});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+    
+  });
